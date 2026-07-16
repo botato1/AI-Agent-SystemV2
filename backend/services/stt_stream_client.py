66555fdaab917247f50888d88ed880507d2dd0f4
zisuu <awt0311@gmail.com>
2026-07-16 19:25:33 +0900

    Fix: STT_STREAM_BASE_URL 주소 수정

--- backend/services/stt_stream_client.py
+++ backend/services/stt_stream_client.py
@@ -7,7 +7,7 @@ from typing import Any, AsyncIterator, Optional
 import websockets
 
 # 8001 실시간 STT WebSocket 서버 주소
-STT_STREAM_BASE_URL = os.getenv("STT_STREAM_BASE_URL", "ws://61.81.98.86:8001")
+STT_STREAM_BASE_URL = os.getenv("STT_STREAM_BASE_URL", "ws://61.81.98.82:8001")
 
 
 class SttStreamClient:
