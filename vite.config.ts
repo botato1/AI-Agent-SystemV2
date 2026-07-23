0d2cd0441dcaa7049cfeba546f86809ce78bc3dc
kxnxyy <nayeonchoi0503@gmail.com>
2026-07-23 10:36:30 +0900

    chore: Vite 환경변수 타입 및 개발 서버 네트워크 접근 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  server: {
+    host: true, // 127.0.0.1, localhost, 내부 IP 등 모든 호스트 접근 허용
+    port: 5173,
+  }
+})
