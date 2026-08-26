d35469d34a19184ddba4a4b939ae01383ab58c72
kxnxyy <nayeonchoi0503@gmail.com>
2026-08-26 17:46:35 +0900

    Chore: 안 쓰는 props/state/import 정리

--- frontend/src/components/Settings.tsx
+++ frontend/src/components/Settings.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Language } from "../data/translations";
 import { ChevronRightIcon, MoonIcon, SunIcon, PlusIcon, PencilIcon, TrashIcon, CheckIcon, CloseIcon, GripIcon } from "./icons";
 import { Theme } from "../hooks/useTheme";
